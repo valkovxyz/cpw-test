e95d7a7cf04d848d6ed9b8888bec60d0558de72c
Artem Valkov <valkov.artyom.fs@gmail.com>
2023-10-11 03:34:28 +0300

    ui

--- src/pages/ConnectWallet/ConnectWallet.tsx
+++ src/pages/ConnectWallet/ConnectWallet.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import Layout from "../../components/layout";
 import connectImage from '../../assets/connect-image.png'
 import cyberpunksworld  from '../../assets/cyberpunksworld.svg';
 import {Button} from "../../components/Button/Button";
 
+
+
 export const ConnectWallet: React.FC = () => {
+
+    const [isWalletConnected, setIsWalletConnected] = useState(false);
+
+    const connectWallet = async () => {
+        try {
+            if (typeof window.ethereum === 'undefined') {
+                throw new Error('MetaMask not detected. Please install MetaMask.');
+            }
+
+            if (window.ethereum.selectedAddress) {
+                // MetaMask is connected, and you have access to the selected address
+                console.log('MetaMask is connected:', window.ethereum.selectedAddress);
+            }
+            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+            console.log(accounts)
+            if (accounts.length > 0) {
+                const userAddress = accounts[0];
+                setIsWalletConnected(true);
+                // Store userAddress or perform other actions
+            } else {
+                // Handle user denial
+            }
+        } catch (error) {
+            console.error('Error connecting wallet:', (error as Error).message);
+        }
+    };
+
     return (
         <Layout>
             <div className={'box'}>
@@ -23,10 +52,12 @@ export const ConnectWallet: React.FC = () => {
                     <Button
                         value={'Connect Wallet'}
                         primary={true}
+                        onClick={connectWallet}
                     />
                     <Button
                         value={'Quick Preview'}
                         primary={false}
+
                     />
                 </div>
             </div>
